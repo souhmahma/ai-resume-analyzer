4488c11d7593cd0e501cedfe0b95ca1245e18239
souhmahma <souhailhmahma@gmail.com>
2026-04-12 02:15:23 +0200

    database_url

--- config/settings.py
+++ config/settings.py
@@ -53,15 +53,15 @@ TEMPLATES = [
     },
 ]
 
-#DATABASES = {
-#    'default': dj_database_url.parse(config('DATABASE_URL'))
-#}
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+    'default': dj_database_url.parse(config('DATABASE_URL'))
 }
+#DATABASES = {
+#    'default': {
+#        'ENGINE': 'django.db.backends.sqlite3',
+#        'NAME': BASE_DIR / 'db.sqlite3',
+#    }
+#}
 
 REST_FRAMEWORK = {
     'DEFAULT_AUTHENTICATION_CLASSES': (
