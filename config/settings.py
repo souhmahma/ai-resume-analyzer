3e07189d51be1b18b6c0f44b3ebf72c50293cf48
Souhail Hmahma <114232619+souhmahma@users.noreply.github.com>
2026-04-12 03:55:35 +0200

    Update email settings for SSL configuration

--- config/settings.py
+++ config/settings.py
@@ -99,8 +99,9 @@ STATIC_ROOT      = BASE_DIR / 'staticfiles'
 # Email
 EMAIL_BACKEND       = config('EMAIL_BACKEND', default='django.core.mail.backends.console.EmailBackend')
 EMAIL_HOST          = config('EMAIL_HOST', default='smtp.gmail.com')
-EMAIL_PORT          = config('EMAIL_PORT', default=587, cast=int)
-EMAIL_USE_TLS       = config('EMAIL_USE_TLS', default=True, cast=bool)
+EMAIL_PORT = 465
+EMAIL_USE_TLS = False
+EMAIL_USE_SSL = True
 EMAIL_HOST_USER     = config('EMAIL_HOST_USER', default='')
 EMAIL_HOST_PASSWORD = config('EMAIL_HOST_PASSWORD', default='')
 DEFAULT_FROM_EMAIL  = config('DEFAULT_FROM_EMAIL', default='noreply@resumeai.com')
